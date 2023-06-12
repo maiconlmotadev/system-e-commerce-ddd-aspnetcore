c7b4db7498c910c05b922968cfa7d5c56d1bdbd4
Maicon Mota <maiconlmotadev@hotmail.com>
2023-06-12 13:54:04 +0100

    Entities: creation of Base and Product classes

---
+++ Entities/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+    public class Product : Notifies
+    {
+        [Column("PRO_ID")]
+        [Display(Name ="Code")]
+        public int ProductId { get; set; }
+
+        [Column("PRO_NAME")]
+        [Display(Name = "Name")]
+        public int Name { get; set; }
+
+        [Column("PRO_VALUE")]
+        [Display(Name = "Value")]
+        public int Value { get; set; }
+
+        [Column("PRO_STATE")]
+        [Display(Name = "State")]
+        public int State { get; set; }
+    }
+}

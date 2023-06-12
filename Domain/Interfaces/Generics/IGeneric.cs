443fdd96638498551621ca4156289d134d2c342b
Maicon Mota <maiconlmotadev@hotmail.com>
2023-06-12 14:27:37 +0100

    iGeneric interface creation

---
+++ Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGeneric<T> where T : class
+    {
+
+    }
+}

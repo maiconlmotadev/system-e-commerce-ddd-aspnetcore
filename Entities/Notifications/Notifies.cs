ae0beeb53c121322f2bf176b9eea84129c3a2961
Maicon Mota <maiconlmotadev@hotmail.com>
2023-06-12 12:29:03 +0100

    Class Notifies: creation and update.

---
+++ Entities/Notifications/Notifies.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+            Notifications = new List<Notifies>();
+        }
+        
+        [NotMapped]
+        public string NameProp { get; set; }
+
+        [NotMapped]
+        public string mensage { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notifications;
+
+        public bool ValidateStringProperty(string value, string nameProperty) 
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifications.Add(new Notifies
+                {
+                    mensage = "Required field!",
+                    NameProp = nameProperty
+                });
+                return false;
+            }  
+            return true;
+        }
+
+
+    }
+}

dd5b7a1f41b54883b1ceb6a6211b2db8dd2ef117
Maicon Mota <maiconlmotadev@hotmail.com>
2023-06-12 14:57:32 +0100

    Criação e implementação: IProduct, IServivceProduct e classe ServiceProduct.

--- Domain/Interfaces/Generics/IGeneric.cs
+++ Domain/Interfaces/Generics/IGeneric.cs
@@ -8,6 +8,12 @@ namespace Domain.Interfaces.Generics
 {
     public interface IGeneric<T> where T : class
     {
+        Task Add(T Object);
+        Task Update(T Object);
+        Task Delete(T Object);
+
+        Task<T> GetEntityById(int Id);
+        Task<T> List();
 
     }
 }
---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+        private readonly IProduct _IProduct;
+        public ServiceProduct(IProduct IProduct) 
+        {
+            _IProduct = IProduct;   
+        }
+
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

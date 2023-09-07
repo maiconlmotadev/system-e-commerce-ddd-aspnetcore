27d638f2c3e85896598f83ae3d767ad2b7f8f023
Maicon Lourenço da Mota <maiconlmotadev@hotmail.com>
2023-09-07 18:11:03 +0100

    Implementation: _AlertPartial.cs; site.js

--- Web_ECommerce/wwwroot/js/site.js
+++ Web_ECommerce/wwwroot/js/site.js
@@ -4,3 +4,27 @@
 // Write your JavaScript code.
 
 
+var AlertObject = new Object();
+
+AlertObject.ViewAlert = function (type, message) {
+    $("JavaScriptAlert").html("");
+
+    // tipo
+    // 1 alert-success
+    // 2 alert-warning
+    // 3 alert-danger
+
+    var AlertClassType = "";
+
+    if (type == 1) {
+        AlertClassType = "alert alert-success";
+    } else if (type == 2) {
+        AlertClassType = "alert alert-warning"
+    } else if (type == 3) {
+        AlertClassType = "alert alert-danger"
+    }
+
+    var AlertDiv = $("<div>", { class: AlertClassType });
+
+}
+
